701fde7e7437238c00d3cd1f16393375c0d10091
Gregory <gred_cal@yahoo.com>
2020-02-19 11:21:16 -0700

    trying to implement the bombs

--- Assignment 2/js/main.js
+++ Assignment 2/js/main.js
@@ -9,13 +9,17 @@ function  makeTable(){
         var row = table.insertRow(y);
         for(var x = 0; x < sizeOfBoard; x++){
             var cell = row.insertCell(x);
+            cell.onclick = function(){
+                click(this);
+            };
             cell.setAttribute("id","tableCell",0);
-            var isMine = document.createElement("mineValue");
+            var isMine = document.createAttribute("mineValue");
             isMine.value = false;
+            cell.setAttributeNode(isMine);
             //cell.setAttribute("class","cell"+x+""+y,0);                   
             cell.innerHTML = x+""+y; 
         }
-    }
+    }addMine();
 }
 function clearBoard(){
     document.getElementById('boardTable').innerHTML="";
@@ -24,6 +28,23 @@ function getBoardSize(){
     var getSize = document.getElementById('boardText').value;
     return getSize;
 }
+function addMine(sizeOfBoard){
+    var theMines = Math.pow(sizeOfBoard,2)*0.2;
+    for (var i = 0; i<theMines; i++){
+        var row = Math.floor(Math.random()*(theMines/2));
+        var col = Math.floor(Math.random()*(theMines/2));
+        var cell = table.rows[row].cells[col];
+        cell.setAttribute("mineValue","true");
+        cell.innerHTML="X";
+    }
+
+}
+
+function click(cell){
+    var cell = table.rows[row].cells[col];
+    cell.innerHTML = "X";
+}
+
 //dunno if I need this
 function tableCell(row,col){
     this.x=row;
