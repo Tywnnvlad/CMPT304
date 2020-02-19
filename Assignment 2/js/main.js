2c75b2cd84bb476c06460ada484abd104a540b35
Gregory <gred_cal@yahoo.com>
2020-02-19 15:50:29 -0700

    making it work

--- Assignment 2/js/main.js
+++ Assignment 2/js/main.js
@@ -12,27 +12,31 @@ function  makeTable(){
             cell.onclick = function(){
                 click(this);
             };
-            cell.setAttribute("id","tableCell",0);
-            var isMine = document.createAttribute("mineValue");
-            isMine.value = false;
-            cell.setAttributeNode(isMine);
+            //Set the coordinates of each tile
+            cell.setAttribute("id","cell"+x+""+y,0);
+            var mine = document.createAttribute("mineValue");
+            mine.value = false;
+            cell.setAttributeNode(mine);
             //cell.setAttribute("class","cell"+x+""+y,0);                   
             cell.innerHTML = x+""+y; 
         }
-    }addMine();
+    }addMine(sizeOfBoard);
 }
+//Clear the board
 function clearBoard(){
     document.getElementById('boardTable').innerHTML="";
 }
+//Get the board Size
 function getBoardSize(){
     var getSize = document.getElementById('boardText').value;
     return getSize;
 }
+//Add mines
 function addMine(sizeOfBoard){
     var theMines = Math.pow(sizeOfBoard,2)*0.2;
     for (var i = 0; i<theMines; i++){
-        var row = Math.floor(Math.random()*(theMines/2));
-        var col = Math.floor(Math.random()*(theMines/2));
+        var row = Math.floor(Math.random()*sizeOfBoard);
+        var col = Math.floor(Math.random()*sizeOfBoard);
         var cell = table.rows[row].cells[col];
         cell.setAttribute("mineValue","true");
         cell.innerHTML="X";
@@ -41,8 +45,9 @@ function addMine(sizeOfBoard){
 }
 
 function click(cell){
-    var cell = table.rows[row].cells[col];
-    cell.innerHTML = "X";
+    if(cell.getAttribute("minveValue")=="true"){
+        alert("BOOM");
+    }
 }
 
 //dunno if I need this
