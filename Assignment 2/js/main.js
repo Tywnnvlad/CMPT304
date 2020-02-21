ae734ae97dfce0a482f0416e2354416ba55eacad
Gregory <gred_cal@yahoo.com>
2020-02-21 14:37:51 -0700

    game working, just need to polish and add functionality

--- Assignment 2/js/main.js
+++ Assignment 2/js/main.js
@@ -1,4 +1,9 @@
-var sizeOfBoard = 5;
+/**
+ Gregory Edward Cal 
+ Assignment #2
+ This program will 
+ */
+var sizeOfBoard = 0;
 var board = document.getElementById('boardDiv');
 var table = document.getElementById('boardTable');
 
@@ -9,7 +14,6 @@ function  makeTable(){
         var row = table.insertRow(y);
         for(var x = 0; x < sizeOfBoard; x++){
             var cell = row.insertCell(x);
-
             //Set the coordinates of each tile
             cell.setAttribute("id","cell"+x+""+y,0);
             var mine = document.createAttribute("mineValue");
@@ -19,7 +23,7 @@ function  makeTable(){
                 click(e.target);
             });
             //cell.setAttribute("class","cell"+x+""+y,0);                   
-            cell.innerHTML = x+""+y; 
+            //cell.innerHTML = x+""+y; 
         }
     }addMine(sizeOfBoard);
 }
@@ -40,11 +44,11 @@ function addMine(sizeOfBoard){
         var col = Math.floor(Math.random()*sizeOfBoard);
         var cell = table.rows[row].cells[col];
         cell.setAttribute("mineValue","true");
-        cell.innerHTML="X";
+        //cell.innerHTML="X";
     }
 
 }
-//Click function
+//Click function that will check if the cell that was clicked has mines
 function click(cell){
     if(cell.getAttribute("mineValue")=="true"){
         cell.classList.add("mine");
@@ -55,7 +59,7 @@ function click(cell){
     checkAround(cell);
     }
 }
-//A function to reveal all the mines if clicked
+//A function to reveal the rest of mines if a mine is clicked. 
 function revealMine(){
     for(y = 0; y < sizeOfBoard; y++){
         for (x = 0; x < sizeOfBoard; x++){
@@ -66,25 +70,28 @@ function revealMine(){
         }
     }
 }
-//
+//check the cell around and replace mine count
 function checkAround(cell){
     var mineCount = 0;
     var cellRow = cell.parentNode.rowIndex;
     var cellCol = cell.cellIndex;
-    for (var i=Math.max(cellRow-1,0); i<=Math.min(cellRow+1,9); i++) {
-        for(var j=Math.max(cellCol-1,0); j<=Math.min(cellCol+1,9); j++) {
+    for (var i=Math.max(cellRow-1,0); i<=Math.min(cellRow+1,sizeOfBoard-1); i++) {
+        for(var j=Math.max(cellCol-1,0); j<=Math.min(cellCol+1,sizeOfBoard-1); j++) {
           if (table.rows[i].cells[j].getAttribute("mineValue")=="true") mineCount++;
         }
       }
       cell.innerHTML=mineCount;
-
+      //If there is zero mine arround, recursively click() around until a mine is found
       if (mineCount==0) { 
-        for (var i=Math.max(cellRow-1,0); i<=Math.min(cellRow+1,9); i++) {
-          for(var j=Math.max(cellCol-1,0); j<=Math.min(cellCol+1,9); j++) {
-            if (grid.rows[i].cells[j].innerHTML=="") clickCell(grid.rows[i].cells[j]);
+        for (var i=Math.max(cellRow-1,0); i<=Math.min(cellRow+1,sizeOfBoard-1); i++) {
+          for(var j=Math.max(cellCol-1,0); j<=Math.min(cellCol+1,sizeOfBoard-1); j++) {
+            if (table.rows[i].cells[j].innerHTML=="") click(table.rows[i].cells[j]);
           }
         }
       }
+
 }
+
+
 
 makeTable();
